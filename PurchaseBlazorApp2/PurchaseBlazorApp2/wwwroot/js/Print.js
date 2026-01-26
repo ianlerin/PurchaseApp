d1e7490263badc3948d485b34e2d6d0b5381fcae
dheneswari <dheneswariv@gmail.com>
2026-01-26 15:51:55 +0800

    print pay slip
    added new print slip button
    added print selected slip function

--- PurchaseBlazorApp2/PurchaseBlazorApp2/wwwroot/js/Print.js
+++ PurchaseBlazorApp2/PurchaseBlazorApp2/wwwroot/js/Print.js
@@ -1,8 +1,10 @@
-﻿window.Print = base64Pdf => {
-    const bytes = Uint8Array.from(atob(base64Pdf), c => c.charCodeAt(0));
-    const blob = new Blob([bytes], { type: "application/pdf" });
-    const url = URL.createObjectURL(blob);
+﻿window.Print = base64List => {
+    base64List.forEach(base64Pdf => {
+        const bytes = Uint8Array.from(atob(base64Pdf), c => c.charCodeAt(0));
+        const blob = new Blob([bytes], { type: "application/pdf" });
+        const url = URL.createObjectURL(blob);
 
-    const win = window.open(url, "_blank");
-    setTimeout(() => win.print(), 500);
+        const win = window.open(url, "_blank");
+        setTimeout(() => win.print(), 300);
+    });
 };
