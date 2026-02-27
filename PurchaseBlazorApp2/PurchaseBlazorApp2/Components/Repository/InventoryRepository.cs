cba37cf7ec352538519ec71a65a4bb22391010de
dheneswari <dheneswariv@gmail.com>
2026-02-27 11:21:09 +0800

    add quantity

--- PurchaseBlazorApp2/PurchaseBlazorApp2/Components/Repository/InventoryRepository.cs
+++ PurchaseBlazorApp2/PurchaseBlazorApp2/Components/Repository/InventoryRepository.cs
@@ -210,7 +210,8 @@ namespace PurchaseBlazorApp2.Components.Repository
               INSERT INTO addrecord (product_id, supplier_id, quantity, created_by)
               VALUES (@product_id, @supplier_id, @quantity, @created_by)
               ON CONFLICT (product_id, supplier_id)
-               DO UPDATE SET quantity = @quantity, created_by = @created_by
+               DO UPDATE SET quantity = addrecord.quantity + EXCLUDED.quantity,
+               created_by = EXCLUDED.created_by
             ", Connection);
 
                 cmd.Parameters.AddWithValue("product_id", record.ItemData.ID);
