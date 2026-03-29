960fe8d9d516193226be2b87f321f0c1fd306a44
ianlerin <32050869+ianlerin@users.noreply.github.com>
2026-03-29 21:02:09 +0800

    1.) Add company state
    1.) Add company state

---
+++ SharedDataType/Service/LoginService.cs
@@ -0,0 +1,32 @@
+﻿using PurchaseBlazorApp2.Components.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedDataType.Service
+{
+    public class CompanyState
+    {
+        public event Action? OnCompanyChanged;
+
+        private CompanyInfo? _currentCompany;
+
+        public CompanyInfo? CurrentCompany
+        {
+            get => _currentCompany;
+            set
+            {
+                _currentCompany = value;
+                OnCompanyChanged?.Invoke();
+            }
+        }
+
+        public void Clear()
+        {
+            _currentCompany = null;
+            OnCompanyChanged?.Invoke();
+        }
+    }
+}
